995955a3d717e904926b085bbb857ae0ee79f314
kju41 <kju41@DESKTOP-TMHJ1KF>
2022-03-12 20:52:31 +0900

    added hello.java

--- src/hello.java
+++ src/hello.java
@@ -2,8 +2,8 @@
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello"); // TODO Auto-generated method stub
+
 	}
 
 }
