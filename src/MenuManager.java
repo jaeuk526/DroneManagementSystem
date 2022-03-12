40fc8981c9a89cf75296c30ebd276b394fa9592c
kju41 <kju41@DESKTOP-TMHJ1KF>
2022-03-12 23:07:48 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Aircraft");
+		System.out.println("2. Delete Aircraft");
+		System.out.println("3. Edit Aircraft");
+		System.out.println("4. View Aircraft");
+		System.out.println("5. Exit");
+
+	}
+
+}
