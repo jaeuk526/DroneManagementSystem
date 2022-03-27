72012889083c6617aefbbfeac86e6cb838ad0984
kju41 <kju41@DESKTOP-TMHJ1KF>
2022-03-27 15:29:13 +0900

    used method instead of switch()

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,19 +1,14 @@
 import java.util.Scanner; //클래스 함수 호출
 
-public class MenuManager{
+public class MenuManager{ //메뉴 관리 클래스
 	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) { //메인함수
 		
-		int num = 0; //num 변수 선언
-		Scanner input = new Scanner(System.in); //input 변수 선언
+		int num = 5; 
+		//초기 설정
 		
-		while (num != 6){
-			
-			int AircraftSerialNum = 0;
-			String AircraftName = null;
-			String AircraftManu = null;
-			//각종 변수들 선언 및 초기화
-			
+		while (num != 6) { //num 입력이 6이 되지 않는한 이 반복문을 계속 실행할것
 			System.out.println("1. Add Aircraft");
 			System.out.println("2. Delete Aircraft");
 			System.out.println("3. Edit Aircraft");
@@ -22,35 +17,49 @@ public class MenuManager{
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1 - 6: ");
 			num = input.nextInt(); //입력값을 num 변수로 설정
-
-			switch(num) {
-			
-			case 1: //기체 추가
-				System.out.print("Aircraft Serial No. "); //기체의 시리얼 넘버
-				AircraftSerialNum = input.nextInt(); //다음 정수 입력값을 AircraftSerialNum 변수로 설정
-				System.out.print("Aircraft Name : "); //기체명
-				AircraftName = input.next(); //입력값을 AircraftName 변수로 설정
-				System.out.print("Aircraft Manufacturer : "); //기체 제작사
-				AircraftManu = input.next(); //입력값을 AircraftManu 변수로 설정
-				break; //Case 1 구문에서 빠져나오기
-			case 2: //기체 제거
-				System.out.print("Enter the Serial Number of the Aircraft to delete : ");
-				AircraftSerialNum = input.nextInt();
-				break; //Case 2 구문에서 빠져나오기
-			case 3: //기체 변경
-				System.out.print("Enter the Serial Number of the Aircraft to edit : ");
-				AircraftSerialNum = input.nextInt();
-				break; //Case 3 구문에서 빠져나오기
-			case 4: //기체 조회
-				System.out.print("Enter the Serial Number of the Aircraft to view : ");
-				AircraftSerialNum = input.nextInt();
-				break; //Case 4 구문에서 빠져나오기
-			case 5: //메뉴
-				break; //Case 5 구문에서 빠져나오기
-			case 6: //종료
-				System.out.print("시스템을 종료합니다");
-				break; //Case 6 구문에서 빠져나오기
+	
+			if (num == 1) { //num이 1로 입력될경우 아래 메소드를 실행
+				addAircraft();
+			}
+			else if (num == 2) {
+				deleteAircraft();
+			}
+			else if (num == 3){
+				editAircraft();
+			}
+			else if (num == 4){
+				viewAircraft();
 			}
 		}
+		
 	}
+	//==============================메소드 파트 시작=================================
+	public static void addAircraft() { //항공기 추가 메소드
+		Scanner input = new Scanner(System.in);
+		System.out.print("Aircraft Serial No. ");
+		int AircraftSerialNum = input.nextInt();
+		System.out.print("Aircraft Name : ");
+		String AircraftName = input.next();
+		System.out.print("Aircraft Manufacturer : ");
+		String AircraftManu = input.next();
+	}
+	
+	public static void deleteAircraft() { //항공가 삭제 메소드
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Serial Number of the Aircraft to delete : ");
+		int AircraftSerialNum = input.nextInt();
+	}
+	
+	public static void editAircraft() { //항공기 편집 메소드
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Serial Number of the Aircraft to edit : ");
+		int AircraftSerialNum = input.nextInt();
+	}
+	
+	public static void viewAircraft() { //항공기 조회 메소드
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Serial Number of the Aircraft to edit : ");
+		int AircraftSerialNum = input.nextInt();
+	}
+	
 }
