be4bc35d1ec88d1a8f1b2e31a1ebd977a2810f3d
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-04-25 22:43:39 +0900

    Update DroneManager.java

--- src/DroneManager.java
+++ src/DroneManager.java
@@ -1,50 +1,112 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import drone.Drone;
+import drone.HelicopterDrone;
+
 public class DroneManager {
-	
-	Drone drone;
+	ArrayList<Drone> drones = new ArrayList<Drone>();
 	Scanner input;
 	
-	DroneManager(Scanner input){ //»ı¼ºÀÚ
-		this.input = input; //¹ŞÀº inputÀ» ¿©±â inputÀ¸·Î µ¿±âÈ­
+	
+	DroneManager(){}
+	DroneManager(Scanner input){
+		this.input = input;
 	}
 	
-	public void addAircraft() { //Ç×°ø±â Ãß°¡ ¸Ş¼Òµå
-		drone = new Drone(); //±âº» »ı¼ºÀÚ »ç¿ë
-		System.out.print("Aircraft Serial No. ");
-		drone.serialNum = input.nextInt();
-		System.out.print("Aircraft Name : ");
-		drone.name = input.next();
-		System.out.print("Aircraft Manufacturer : ");
-		drone.manu = input.next();
+	public void addAircraft() { //í•­ê³µê¸° ì¶”ê°€ ë©”ì†Œë“œ
+		int kind = 0;
+		Drone drone;
+		
+		while (kind != 1 && kind != 2) {
+			System.out.print("1 for Multicopter ");
+			System.out.print("2 for Helicopter ");
+			System.out.print("Select num for Drone Type between 1 and 2: ");
+			kind = input.nextInt();
+			if (kind == 1) {
+				drone = new Drone();
+				drone.getUserInput(input);
+				drones.add(drone);
+				break;
+			}
+			else if (kind == 2) {
+				drone = new HelicopterDrone();
+				drone.getUserInput(input);
+				drones.add(drone);
+				break; 
+			}
+			else {
+				System.out.print("Select num for Drone Type");
+			}
+		}
 	}
 	
-	public void deleteAircraft() { //Ç×°ø°¡ »èÁ¦ ¸Ş¼Òµå
+	public void deleteAircraft() { //í•­ê³µê¸° ì‚­ì œ ë©”ì†Œë“œ
 		System.out.print("Enter the Serial Number of the Aircraft to delete : ");
 		int AircraftSerialNum = input.nextInt();
-		if (drone == null) { //¸¸¾à ÀÔ·ÂµÈ °ªÀÌ ¾Æ¹«°Íµµ ¾øÀ»°æ¿ì
-			System.out.println("the drone has not been registered");
-			return;
+		int index = -1; // array ì—ì„œ indexë¥¼ ëª»ì°¾ì•˜ë‹¤ëŠ” ì˜ë¯¸
+		for (int i = 0; i < drones.size(); i++) {
+			if (drones.get(i).getSerialNum() == AircraftSerialNum) {
+				index = i;
+				break;
+			}
+			else {
+				System.out.println("the drone has not been registered");
+				return;
+			}
 		}
-		if (AircraftSerialNum == drone.serialNum) { //ÀÔ·ÂµÈ °ªÀÌ ±âÁ¸ÀÇ °ª°ú ÀÏÄ¡ÇÒ °Ü¿ì
-			drone = null; //Á¤º¸ »èÁ¦
-			System.out.println("the drone is deleted");
+		
+		if (index >= 0) {
+			drones.remove(index);
+			System.out.println("The drone " + AircraftSerialNum + " is deleted");
 		}
 	}
 	
-	public void editAircraft() { //Ç×°ø±â ÆíÁı ¸Ş¼Òµå
+	public void editAircraft() { //í•­ê³µê¸° í¸ì§‘ ë©”ì†Œë“œ
 		System.out.print("Enter the Serial Number of the Aircraft to edit : ");
+		
 		int AircraftSerialNum = input.nextInt();
-		if (drone.serialNum == AircraftSerialNum) {
-			System.out.println("the drone to be edited is " + drone.serialNum);
+		for(int i = 0; i < drones.size(); i++) {
+			Drone drone = drones.get(i);
+			if (drone.getSerialNum() == AircraftSerialNum) {
+				int num = -1;
+				while(num != 5) {
+					System.out.println("** Drone Info Edit Menu **");
+					System.out.println(" 1. Edit Serial Number");
+					System.out.println(" 2. Edit Name");
+					System.out.println(" 3. Edit Manufacturer");
+					System.out.println(" 4. Exit");
+					System.out.println("Select one number between 1 - 4");
+					num = input.nextInt();
+					if(num == 1) {
+						System.out.println("Drone Serial Number: ");
+						drone.setSerialNum(input.nextInt());
+					}
+					else if(num == 2) {
+						System.out.println("Drone Name: ");
+						drone.setName(input.next());
+					}
+					else if(num == 3) {
+						System.out.println("Drone Manufacturer: ");
+						drone.setManu(input.next());
+					}
+					else if(num == 4) {
+						break;
+					}
+					else {
+						continue;
+					}
+				
+				}
+				break;
+			}
 		}
 	}
 	
-	public void viewAircraft() { //Ç×°ø±â Á¶È¸ ¸Ş¼Òµå
-		System.out.print("Enter the Serial Number of the Aircraft to edit : ");
-		int AircraftSerialNum = input.nextInt();
-		if (drone.serialNum == AircraftSerialNum) {
-			drone.printInfo(); //µå·Ğ Á¤º¸ Ãâ·Â
+	public void viewAircrafts() { //í•­ê³µê¸° ì¡°íšŒ ë©”ì†Œë“œ
+		System.out.println("# of registered drones: " + drones.size());
+		for (int i = 0; i < drones.size(); i++) {
+			drones.get(i).printInfo();
 		}
 	}
 }
