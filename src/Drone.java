1e0492af3fb272f81c929de3e7353a8724d0b311
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-05-02 23:04:32 +0900

    Update Drone.java

--- src/Drone.java
+++ src/Drone.java
@@ -1,67 +1,109 @@
-package drone;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Drone {
-	protected DroneType type = DroneType.Multicoptor; //캡슐화
-	protected String name;
-	protected int serialNum;
-	protected String manu;
+public class DroneManager {
+	ArrayList<Drone> drones = new ArrayList<Drone>();
+	Scanner input;
 	
-	public DroneType getType() {
-		return type;
+	
+	DroneManager(){}
+	DroneManager(Scanner input){
+		this.input = input;
 	}
 	
-	public void setType(DroneType type) {
-		this.type = type;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getSerialNum() {
-		return serialNum;
-	}
-	
-	public void setSerialNum(int serialNum) {
-		this.serialNum = serialNum;
-	}
-	
-	public String getManu() {
-		return manu;
-	}
-	
-	public void setManu(String manu) {
-		this.manu = manu;
-	}
-
-	
-	public Drone(){}
-	public Drone(String name, int serialNum, String manu){ //생성자
-		this.name = name;
-		this.serialNum = serialNum;
-		this.manu = manu;
-	}
-	
-	public void printInfo(){ //정보 출력
-		System.out.println("name: " + name + " serialNum: " + serialNum + " maufacturer: " + manu);
-	}
-	
-	public void getUserInput(Scanner input) {
-		System.out.print("Aircraft Serial No. ");
-		int serialNum = input.nextInt();
-		this.setSerialNum(serialNum);
+	public void addAircraft() { //항공기 추가 메소드
+		int type = 0;
+		Drone drone;
 		
-		System.out.print("Aircraft Name : ");
-		String name = input.next();
-		this.setName(name);
+		while (type != 1 && type != 2) {
+			System.out.print("1 for Multicopter ");
+			System.out.println("2 for Helicopter ");
+			System.out.print("Select number for Drone Type between 1 and 2: ");
+			type = input.nextInt();
+			if (type == 1) { //멀티콥터의 경우
+				drone = new Drone();
+				drone.getUserInput(input);
+				drones.add(drone);
+				break;
+			}
+			else if (type == 2) { //헬리콥터의 경우 
+				drone = new HelicopterDrone();
+				drone.getUserInput(input);
+				drones.add(drone);
+				break;
+			}
+			else {
+				System.out.print("Select number for Drone Type");
+			}
+		}
+	}
+	
+	public void deleteAircraft() { //항공기 삭제 메소드
+		System.out.print("Enter the Serial Number of the Aircraft to delete : ");
+		int AircraftSerialNum = input.nextInt();
+		int index = -1; // array 에서 index를 못찾았다는 의미
+		for (int i = 0; i < drones.size(); i++) {
+			if (drones.get(i).getSerialNum() == AircraftSerialNum) {
+				index = i;
+				break;
+			}
+			else {
+				System.out.println("the drone has not been registered");
+				return;
+			}
+		}
 		
-		System.out.print("Aircraft Manufacturer : ");
-		String manu = input.next();
-		this.setManu(manu);
+		if (index >= 0) {
+			drones.remove(index);
+			System.out.println("The drone " + AircraftSerialNum + " is deleted");
+		}
+	}
+	
+	public void editAircraft() { //항공기 편집 메소드
+		System.out.print("Enter the Serial Number of the Aircraft to edit : ");
+		
+		int AircraftSerialNum = input.nextInt();
+		for(int i = 0; i < drones.size(); i++) {
+			Drone drone = drones.get(i);
+			if (drone.getSerialNum() == AircraftSerialNum) {
+				int num = -1;
+				while(num != 5) {
+					System.out.println("** Drone Info Edit Menu **");
+					System.out.println(" 1. Edit Serial Number");
+					System.out.println(" 2. Edit Name");
+					System.out.println(" 3. Edit Manufacturer");
+					System.out.println(" 4. Exit");
+					System.out.println("Select one number between 1 - 4");
+					num = input.nextInt();
+					if(num == 1) {
+						System.out.println("Drone Serial Number: ");
+						drone.setSerialNum(input.nextInt());
+					}
+					else if(num == 2) {
+						System.out.println("Drone Name: ");
+						drone.setName(input.next());
+					}
+					else if(num == 3) {
+						System.out.println("Drone Manufacturer: ");
+						drone.setManu(input.next());
+					}
+					else if(num == 4) {
+						break;
+					}
+					else {
+						continue;
+					}
+				
+				}
+				break;
+			}
+		}
+	}
+	
+	public void viewAircrafts() { //항공기 조회 메소드
+		System.out.println("# of registered drones: " + drones.size());
+		for (int i = 0; i < drones.size(); i++) {
+			drones.get(i).printInfo();
+		}
 	}
 }
