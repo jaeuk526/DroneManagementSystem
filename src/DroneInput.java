6dd3daf410dc242998bd04f74980405d15c86f3d
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-05-09 15:22:23 +0900

    Add files via upload

---
+++ src/DroneInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public interface DroneInput {
+	public void getUserInput(Scanner input);
+	
+	public void setSerialNum(int serialNum);
+	
+	public void setName(String name);
+	
+	public void setManu(String manu);
+}
