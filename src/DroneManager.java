ad82dd16f5dd7e99ab6bea13e1aad3339fa2e0ba
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-05-16 02:17:28 +0900

    Update DroneManager.java

--- src/DroneManager.java
+++ src/DroneManager.java
@@ -41,21 +41,30 @@ public class DroneManager {
 	public void deleteAircraft() { //항공기 삭제 메소드
 		System.out.print("Enter the Serial Number of the Aircraft to delete : ");
 		int AircraftSerialNum = input.nextInt();
+		int index = findIndex(AircraftSerialNum);
+		removeFromDrones(index, AircraftSerialNum);
+	}
+	
+	public int findIndex(int AircraftSerialNum) {
 		int index = -1; // array 에서 index를 못찾았다는 의미
 		for (int i = 0; i < drones.size(); i++) {
 			if (drones.get(i).getSerialNum() == AircraftSerialNum) {
 				index = i;
 				break;
 			}
-			else {
-				System.out.println("the drone has not been registered");
-				return;
-			}
 		}
-		
+		return index;
+	}
+	
+	public int removeFromDrones(int index,int AircraftSerialNum) {
 		if (index >= 0) {
 			drones.remove(index);
 			System.out.println("The drone " + AircraftSerialNum + " is deleted");
+			return 1;
+		}
+		else {
+			System.out.println("the drone has not been registered");
+			return -1;
 		}
 	}
 	
@@ -64,33 +73,26 @@ public class DroneManager {
 		
 		int AircraftSerialNum = input.nextInt();
 		for(int i = 0; i < drones.size(); i++) {
-			DroneInput droneInput = drones.get(i);
-			if (droneInput.getSerialNum() == AircraftSerialNum) {
+			DroneInput drone = drones.get(i);
+			if (drone.getSerialNum() == AircraftSerialNum) {
 				int num = -1;
 				while(num != 5) {
-					System.out.println("** Drone Info Edit Menu **");
-					System.out.println(" 1. Edit Serial Number");
-					System.out.println(" 2. Edit Name");
-					System.out.println(" 3. Edit Manufacturer");
-					System.out.println(" 4. Exit");
-					System.out.println("Select one number between 1 - 4");
+					showEditMenu();
 					num = input.nextInt();
-					if(num == 1) {
-						System.out.println("Drone Serial Number: ");
-						droneInput.setSerialNum(input.nextInt());
-					}
-					else if(num == 2) {
-						System.out.println("Drone Name: ");
-						droneInput.setName(input.next());
-					}
-					else if(num == 3) {
-						System.out.println("Drone Manufacturer: ");
-						droneInput.setManu(input.next());
-					}
-					else if(num == 4) {
+					
+					switch(num) {
+					case 1:
+						drone.setDroneSN(input); //droneInput > drone
 						break;
-					}
-					else {
+					case 2:
+						drone.setDroneName(input);
+						break;
+					case 3:
+						drone.setDroneManu(input);
+						break;
+					case 4:
+						break;
+					default:
 						continue;
 					}
 				
@@ -105,5 +107,15 @@ public class DroneManager {
 		for (int i = 0; i < drones.size(); i++) {
 			drones.get(i).printInfo();
 		}
+	}
+	
+	
+	public void showEditMenu() { //메뉴화면 표시 메소드
+		System.out.println("** Drone Info Edit Menu **");
+		System.out.println(" 1. Edit Serial Number");
+		System.out.println(" 2. Edit Name");
+		System.out.println(" 3. Edit Manufacturer");
+		System.out.println(" 4. Exit");
+		System.out.println("Select one number between 1 - 4");
 	}
 }
