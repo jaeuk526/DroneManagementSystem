b93ca2360c733330eb76e5e38de4ae421e183b8a
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-05-16 17:30:53 +0900

    Update Drone.java

--- src/Drone.java
+++ src/Drone.java
@@ -5,6 +5,7 @@ public abstract class Drone implements DroneInput{
 	protected String name;
 	protected int serialNum;
 	protected String manu;
+	protected String email;
 	
 	/*getter, setter method*/
 	public DroneType getType() {
@@ -38,21 +39,32 @@ public abstract class Drone implements DroneInput{
 	public void setManu(String manu) {
 		this.manu = manu;
 	}
+	
+	public void setEmail(String email) throws EmailFormatException {
+		if (!email.contains("@") && !email.equals("")) {
+			throw new EmailFormatException();
+		}
+		this.email = email;
+	}
 
 	
 	public Drone(){}
-	public Drone(String name, int serialNum, String manu){
+	public Drone(String name, int serialNum, String manu,String email){
 		this.name = name;
 		this.serialNum = serialNum;
 		this.manu = manu;
+		this.email = email;
 	}
 	
 	public void getUserInput(Scanner input) {
 		setDroneSN(input);
 		setDroneName(input);
 		setDroneManu(input);
+		setEmail(input);
 		System.out.println("");
 	}
+	
+	
 	
 	public void setDroneSN(Scanner input) {
 		System.out.print("Aircraft Serial No. ");
@@ -62,13 +74,26 @@ public abstract class Drone implements DroneInput{
 	
 	public void setDroneName(Scanner input) {
 		System.out.println("Drone Name: ");
-		int name = input.nextInt();
-		this.setSerialNum(name);
+		String name = input.next();
+		this.setName(name);
 	}
 	
 	public void setDroneManu(Scanner input) {
 		System.out.println("Drone Manufacturer: ");
-		int manufacturer = input.nextInt();
-		this.setSerialNum(manufacturer);
+		String manufacturer = input.next();
+		this.setManu(manufacturer);
+	}
+	
+	public void setEmail(Scanner input) {
+		String email = "";
+		while (!email.contains("@") || email.equals("")) {
+			System.out.println("Email: ");
+			email = input.next();
+			try {
+				this.setEmail(email);
+			} catch(EmailFormatException e) {
+				System.out.println("Incorrect Email Format, put the e-mail address that contains @");
+			}
+		}
 	}
 }
