9c15ea02308d32f2dfa30160ef5835ee28124324
jaeuk526 <101454940+jaeuk526@users.noreply.github.com>
2022-05-16 17:33:10 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,34 +1,49 @@
-import java.util.Scanner;
+
+import java.util.*;
 
 public class MenuManager{
 
 	public static void main(String[] args) { //메인함수
 		Scanner input = new Scanner(System.in);
 		DroneManager droneManager = new DroneManager(input);
+		selectMenu(input,droneManager);
+		
+	}
+	
+	public static void selectMenu(Scanner input,DroneManager droneManager) {
 		int num = -1;
 
 		while (num != 5) {
-			showMenu(); //메뉴함수를 따로 생성
-			num = input.nextInt();
-
-			switch(num) { //if else 문 대신 switch문을 사용
-			case 1:
-				droneManager.addAircraft();
-				break;
-			case 2:
-				droneManager.deleteAircraft();
-				break;
-			case 3:
-				droneManager.editAircraft();
-				break;
-			case 4:
-				droneManager.viewAircrafts();
-				break;
-			default:
-				continue;
+			
+			try {
+				showMenu(); //메뉴함수를 따로 생성
+				num = input.nextInt();
+	
+				switch(num) { //if else 문 대신 switch문을 사용
+				case 1:
+					droneManager.addAircraft();
+					break;
+				case 2:
+					droneManager.deleteAircraft();
+					break;
+				case 3:
+					droneManager.editAircraft();
+					break;
+				case 4:
+					droneManager.viewAircrafts();
+					break;
+				default:
+					continue;
+				}
+			}
+			catch(InputMismatchException e){
+				System.out.println("please put an integer between 1 and 5");
+				if (input.hasNext()) {
+					input.next();
+				}
+				num = -1;
 			}
 		}
-		
 	}
 	
 	public static void showMenu() {
